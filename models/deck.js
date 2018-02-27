89d1a0a40ef24f0e63560e9e1993c0ffa2d6af17
William Graham <w.m.graham24@gmail.com>
2018-02-27 12:11:14 +0000

    added deck schema

---
+++ models/deck.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const deckSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  level: { type: String, required: true },
+  language: { type: String, required: true },
+  cards: [],
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' }
+});
+
+// modifying the JSON output:
+deckSchema.set('toJSON', {
+  getters: true,
+  virtuals: true,
+  transform(obj, json) {
+    delete json._id;
+    delete json.__v;
+  }
+});
+
+module.exports = mongoose.model('Deck', deckSchema);
