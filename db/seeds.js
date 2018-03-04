617398649cbf8a378c4e778c48747d7a16006170
William Graham <w.m.graham24@gmail.com>
2018-03-04 12:43:03 +0000

    added number cards to deck seeds

--- db/seeds.js
+++ db/seeds.js
@@ -38,6 +38,46 @@ User
     return Deck
       .create([{
         name: 'Numbers',
+        language: 'Spanish',
+        cards: [{
+          question: '1',
+          answer: 'Uno'
+        },{
+          question: '2',
+          answer: 'Dos'
+        },{
+          question: '3',
+          answer: 'Tres'
+        },{
+          question: '4',
+          answer: 'Cuatro'
+        },{
+          question: '5',
+          answer: 'Cinco'
+        }],
+        createdBy: users[2]._id
+      },{
+        name: 'Numbers',
+        language: 'Italian',
+        cards: [{
+          question: '1',
+          answer: 'Uno'
+        },{
+          question: '2',
+          answer: 'Due'
+        },{
+          question: '3',
+          answer: 'Tre'
+        },{
+          question: '4',
+          answer: 'Quattro'
+        },{
+          question: '5',
+          answer: 'Cinque'
+        }],
+        createdBy: users[2]._id
+      },{
+        name: 'Numbers',
         language: '',
         cards: [{
           question: '1',
@@ -56,7 +96,87 @@ User
           answer: ''
         }],
         createdBy: users[2]._id
-      } ]);
+      },{
+        name: 'Numbers',
+        language: '',
+        cards: [{
+          question: '1',
+          answer: ''
+        },{
+          question: '2',
+          answer: ''
+        },{
+          question: '3',
+          answer: ''
+        },{
+          question: '4',
+          answer: ''
+        },{
+          question: '5',
+          answer: ''
+        }],
+        createdBy: users[2]._id
+      },{
+        name: 'Numbers',
+        language: '',
+        cards: [{
+          question: '1',
+          answer: ''
+        },{
+          question: '2',
+          answer: ''
+        },{
+          question: '3',
+          answer: ''
+        },{
+          question: '4',
+          answer: ''
+        },{
+          question: '5',
+          answer: ''
+        }],
+        createdBy: users[2]._id
+      },{
+        name: 'Numbers',
+        language: '',
+        cards: [{
+          question: '1',
+          answer: ''
+        },{
+          question: '2',
+          answer: ''
+        },{
+          question: '3',
+          answer: ''
+        },{
+          question: '4',
+          answer: ''
+        },{
+          question: '5',
+          answer: ''
+        }],
+        createdBy: users[2]._id
+      },{
+        name: 'Numbers',
+        language: '',
+        cards: [{
+          question: '1',
+          answer: ''
+        },{
+          question: '2',
+          answer: ''
+        },{
+          question: '3',
+          answer: ''
+        },{
+          question: '4',
+          answer: ''
+        },{
+          question: '5',
+          answer: ''
+        }],
+        createdBy: users[2]._id
+      }, ]);
   })
   .then((decks) => {
     // console.log(decks);
