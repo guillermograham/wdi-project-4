93aa212c30bd2809b8d1a23749e920a008b441f7
William Graham <w.m.graham24@gmail.com>
2018-03-04 09:17:37 +0000

    added flags to DecksIndex

--- src/components/decks/DecksIndex.js
+++ src/components/decks/DecksIndex.js
@@ -62,7 +62,7 @@ class DecksIndex extends Component {
               <Link to={`/decks/${deck.id}`}>
               <div className="card-content">
                 <h3 className="title is-4">{deck.name}</h3>
-                <img className="flag" src="https://openclipart.org/image/360px/svg_to_png/250015/BevelledFrance.png&disposition=attachment" alt="France flag (bevelled)" title="France flag (bevelled) by  Firkin ( https://openclipart.org/user-detail/Firkin )" />
+                <img className="flag" src={`../../../assets/images/flags/${deck.language}.svg`} alt="France flag (bevelled)" title="France flag (bevelled) by  Firkin ( https://openclipart.org/user-detail/Firkin )" />
                 <p><i className="far fa-flag"></i> {deck.language}</p>
                 { deck.favourites.length !== 1 && <p><i className="far fa-star"></i> {deck.favourites.length} favourites</p>}
                 { deck.favourites.length === 1 && <p><i className="far fa-star"></i> {deck.favourites.length} favourite</p>}
