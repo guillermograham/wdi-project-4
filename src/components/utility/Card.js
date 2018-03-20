007240c7d8c416c96cc039c6ad8aa59eed6bb792
William Graham <w.m.graham24@gmail.com>
2018-03-20 16:26:28 +0000

    refactored card

--- src/components/utility/Card.js
+++ src/components/utility/Card.js
@@ -8,12 +8,27 @@ const Card = ({ deck }) => {
       <Link to={`/decks/${deck.id}`}>
         <div className="card-content">
           <h3 className="title is-4">{deck.name}</h3>
-          <img className="flag" src={`../../../assets/images/flags/${deck.language}.svg`} alt={`${deck.language} flag`} title={`${deck.language} flag`} />
+          <img
+            className="flag"
+            src={`../../../assets/images/flags/${deck.language}.svg`}
+            alt={`${deck.language} flag`}
+            title={`${deck.language} flag`}
+          />
           <p><i className="far fa-flag"></i> {deck.language}</p>
-          { deck.favourites && <div>
-            { deck.favourites.length !== 1 && <p><i className="far fa-star"></i> {deck.favourites.length} favourites</p>}
-            { deck.favourites.length === 1 && <p><i className="far fa-star"></i> {deck.favourites.length} favourite</p>}
-          </div>}
+          { deck.favourites &&
+            <div>
+              { deck.favourites.length !== 1 &&
+                <p>
+                  <i className="far fa-star"></i> {deck.favourites.length} favourites
+                </p>
+              }
+              { deck.favourites.length === 1 &&
+                <p>
+                  <i className="far fa-star"></i> {deck.favourites.length} favourite
+                </p>
+              }
+            </div>
+          }
         </div>
       </Link>
     </div>
