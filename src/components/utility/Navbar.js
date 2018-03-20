dcf66579e736878276617c9f032d58fd113c8e32
William Graham <w.m.graham24@gmail.com>
2018-03-20 16:33:34 +0000

    refactored code on navbar

--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -14,42 +14,67 @@ const Navbar = ({ history, toggleBurger, showBurger }) => {
   return(
     <nav className="navbar is-transparent container" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item"><h1>wordUp</h1></Link>
-        <div className={`${(showBurger ? 'is-active' : '')} navbar-burger burger`} data-target="nav-menu" onClick={toggleBurger}>
+        <Link to="/" className="navbar-item">
+          <h1>wordUp</h1>
+        </Link>
+        <div
+          className={`${(showBurger ? 'is-active' : '')} navbar-burger burger`}
+          data-target="nav-menu"
+          onClick={toggleBurger}
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <div className={`${(showBurger ? 'is-active' : '')} navbar-menu`} onClick={toggleBurger} id="nav-menu">
+      <div
+        className={`${(showBurger ? 'is-active' : '')} navbar-menu`}
+        onClick={toggleBurger}
+        id="nav-menu"
+      >
         <div className="navbar-start">
           <div className="navbar-item">
-            { Auth.isAuthenticated() && <Link to={`/users/${Auth.getPayload().userId}`}><i className="far fa-user"></i> Profile</Link> }
+            { Auth.isAuthenticated() &&
+              <Link to={`/users/${Auth.getPayload().userId}`}>
+                <i className="far fa-user"></i> Profile
+              </Link>
+            }
           </div>
           <div className="navbar-item">
-            { Auth.isAuthenticated() && <Link to="/decks"><i className="fas fa-book"></i> Decks</Link> }
+            { Auth.isAuthenticated() &&
+              <Link to="/decks">
+                <i className="fas fa-book"></i> Decks
+              </Link>
+            }
           </div>
           <div className="navbar-item">
-            { Auth.isAuthenticated() && <Link to="/decks/new"><i className="far fa-file"></i> Create</Link> }
+            { Auth.isAuthenticated() &&
+              <Link to="/decks/new">
+                <i className="far fa-file"></i> Create
+              </Link>
+            }
           </div>
         </div>
         <div className="navbar-end">
           <div className="navbar-item">
-            { Auth.isAuthenticated() && <a href="#" className="" onClick={logout}><i className="fas fa-sign-out-alt"></i> Logout</a> }
+            { Auth.isAuthenticated() &&
+              <a href="#" onClick={logout}>
+                <i className="fas fa-sign-out-alt"></i> Logout
+              </a>
+            }
           </div>
           <div className="navbar-item">
-            { !Auth.isAuthenticated() && <Link to="/login" className="">Login</Link> }
+            { !Auth.isAuthenticated() &&
+              <Link to="/login">Login</Link> }
           </div>
           <div className="navbar-item">
-            { !Auth.isAuthenticated() && <Link to="/register" className="">Register</Link> }
+            { !Auth.isAuthenticated() &&
+              <Link to="/register">Register</Link> }
           </div>
         </div>
       </div>
-
-
     </nav>
   );
 };
 
-// browser router will send the global history
 export default withRouter(Navbar);
